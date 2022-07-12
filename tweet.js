562f18e8265342f83e9877ef3778bb324c789cd4
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-07-12 14:48:47 -0600

    initial answers

--- tweet.js
+++ tweet.js
@@ -1,5 +1,26 @@
 class Tweet {
+  constructor(userName, tweetText, link) {
+    this.sender = userName
+    this.content = tweetText
+    this.link = link || "no link provided"
+    this.likes = 0
+    this.retweets = 0
+    this.replies = []
+    this.replyCount = 0
+    this.likes = 0
+}
+like() {
+  this.likes++
+}
+retweet(){
+  this.retweets++
+}
+reply(expected){
+this.replies.push(expected)
 
 }
+}
+
+
 
 module.exports = Tweet;
